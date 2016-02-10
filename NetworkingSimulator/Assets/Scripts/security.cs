618de5cceec0a4d1069569e0e97d001e39c37062
Zeeshan Sajid <mrzeeshank@gmail.com>
2016-02-10 01:13:17 -0800

    Added starting code for the gate detection; something weird with gate position in which ray is visible but will not detect collision accurately

--- NetworkingSimulator/Assets/Scripts/security.cs
+++ NetworkingSimulator/Assets/Scripts/security.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class security : MonoBehaviour {
 	Vector3 Position;
 
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +12,18 @@ public class security : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Vector3 fwd = transform.TransformDirection (Vector3.left) * 30;
+		Vector3 fd = transform.TransformDirection (Vector3.left);
+		Vector3 wd = transform.TransformDirection (Vector3.forward);
+
+
+		if (Physics.Raycast (transform.position, fd, 80)) {
+			print ("f");
+			Debug.DrawRay (transform.position, fwd, Color.green);
+			transform.Translate(new Vector3 (0, 0, 0)) ;
+		}
+
+	
 	
 	}
 
