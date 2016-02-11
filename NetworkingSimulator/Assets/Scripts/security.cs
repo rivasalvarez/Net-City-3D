4a541b022082e4a64bb9187cd14f8fae0c90ef36
Zeeshan Sajid <mrzeeshank@gmail.com>
2016-02-10 21:24:06 -0800

    I finished the security gate detection for car, but need rb for each car along with box collider

--- NetworkingSimulator/Assets/Scripts/security.cs
+++ NetworkingSimulator/Assets/Scripts/security.cs
@@ -12,15 +12,19 @@ public class security : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 fwd = transform.TransformDirection (Vector3.left) * 30;
+		Vector3 fwd = transform.TransformDirection (Vector3.left) * 80;
 		Vector3 fd = transform.TransformDirection (Vector3.left);
-		Vector3 wd = transform.TransformDirection (Vector3.forward);
+		Vector3 wd = transform.TransformDirection (Vector3.forward) * 80;
 
 
-		if (Physics.Raycast (transform.position, fd, 80)) {
-			print ("f");
-			Debug.DrawRay (transform.position, fwd, Color.green);
-			transform.Translate(new Vector3 (0, 0, 0)) ;
+		Debug.DrawRay (transform.position, wd, Color.green);
+		RaycastHit hit;
+		Ray landingRay = new Ray (transform.position, Vector3.forward);
+		if (Physics.Raycast (landingRay, out hit, Mathf.Infinity)) {
+			print (hit);
+			Debug.DrawRay (transform.position , wd, Color.green);
+
+			transform.Translate(new Vector3 (90,20,30)) ;
 		}
 
 	
