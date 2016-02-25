36df3a212e9669e40f0ec4a59c1084e3bd721dda
Zeeshan <mrzeeshank@gmail.com>
2016-02-25 10:03:42 -0800

    Added stuff for the security to check the angle of the ray

--- NetworkingSimulator/Assets/Scripts/security.cs
+++ NetworkingSimulator/Assets/Scripts/security.cs
@@ -12,6 +12,7 @@ public class Security : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// Three different vectors for different angles the object is placed
 		Vector3 fwd = transform.TransformDirection (Vector3.left) * 80;
 		Vector3 fd = transform.TransformDirection (Vector3.left);
 		Vector3 wd = transform.TransformDirection (Vector3.forward) * 80;
@@ -20,12 +21,34 @@ public class Security : MonoBehaviour {
 		Debug.DrawRay (transform.position, wd, Color.green);
 		RaycastHit hit;
 		Ray landingRay = new Ray (transform.position, Vector3.forward);
-		if (Physics.Raycast (landingRay, out hit, Mathf.Infinity)) {
-			print (hit);
-			Debug.DrawRay (transform.position , wd, Color.green);
 
-			transform.Translate(new Vector3 (90,20,30)) ;
-		}
+		// Check for the rotation of the object
+		//if(transform.eulerAngles.y == )
+			//{
+				// Check if the measurement is not-changed, then shoot out a raycast straight
+				if (Physics.Raycast (landingRay, out hit, Mathf.Infinity)) {
+					print (hit);
+					Debug.DrawRay (transform.position , wd, Color.green);
+
+					transform.Translate(new Vector3 (90,20,30)) ;
+				}
+
+				// Otherwise check if the rotation is positioned in 90 degrees
+				if (Physics.Raycast (landingRay, out hit, Mathf.Infinity)) {
+						print (hit);
+						Debug.DrawRay (transform.position , wd, Color.green);
+
+						transform.Translate(new Vector3 (90,20,30)) ;
+					}
+
+				// Otherwise check if the rotation is position in the -180
+				if (Physics.Raycast (landingRay, out hit, Mathf.Infinity)) {
+						print (hit);
+						Debug.DrawRay (transform.position , wd, Color.green);
+
+						transform.Translate(new Vector3 (90,20,30)) ;
+					}
+			//}
 
 	
 	
